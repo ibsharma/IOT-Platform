767008f5be1d305034cc1e7f8a4a9d2ae4ff7f3c
ibsharma <ishaan_0990@yahoo.com>
2016-08-14 11:36:27 -0500

    Project Structure

---
+++ src/main/java/com/example/dao/Impl/AlertsDAOImpl.java
@@ -0,0 +1,7 @@
+package com.example.dao.Impl;
+
+/**
+ * @author Ishaan
+ */
+public class AlertsDAOImpl {
+}
---
+++ src/main/java/com/example/dao/MetricsDAO.java
@@ -0,0 +1,7 @@
+package com.example.dao;
+
+/**
+ * @author Ishaan
+ */
+public interface MetricsDAO {
+}
